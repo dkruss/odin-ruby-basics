8e2204c00b3dfcbe7d192d20b63bed80e80ca004
David Kruss <davidis1337@gmail.com>
2025-07-29 01:13:54 +1000

    Add initial linked list files

---
+++ linked-lists/lib/linked_list.rb
@@ -0,0 +1,47 @@
+require_relative 'node'
+
+class LinkedList
+  def initialize
+    #TODO
+  end
+
+  def append(value)
+    #TODO
+  end
+
+  def prepend(value)
+    #TODO
+  end
+
+  def size
+    #TODO
+  end
+
+  def head
+    #TODO
+  end
+
+  def tail
+    #TODO
+  end
+
+  def at(index)
+    #TODO
+  end
+
+  def pop
+    #TODO
+  end
+
+  def contains?(value)
+    #TODO
+  end
+
+  def find(value)
+    #TODO
+  end
+
+  def to_s
+    #TODO
+  end
+end

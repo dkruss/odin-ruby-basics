a3b845f5962ca3015dd329a9e05b4768f3577344
David Kruss <davidis1337@gmail.com>
2025-07-30 00:57:28 +1000

    Build methods and add tests

--- linked-lists/lib/linked_list.rb
+++ linked-lists/lib/linked_list.rb
@@ -1,47 +1,96 @@
 require_relative 'node'
 
 class LinkedList
+  attr_accessor :node_list
+
   def initialize
-    #TODO
+    @node_list = []
   end
 
   def append(value)
-    #TODO
+    new_node = Node.new(value)
+    unless self.node_list.empty?
+      self.node_list[-1].next_node = new_node
+    end
+    self.node_list << new_node
   end
 
   def prepend(value)
-    #TODO
+    new_node = Node.new(value)
+    unless self.node_list.empty?
+      new_node.next_node = self.node_list[0]
+    end
+    self.node_list.unshift(new_node)
   end
 
   def size
-    #TODO
+    self.node_list.length
   end
 
   def head
-    #TODO
+    self.node_list[0]
   end
 
   def tail
-    #TODO
+    self.node_list[-1]
   end
 
   def at(index)
-    #TODO
+    self.node_list[index]
   end
 
   def pop
-    #TODO
+    self.pop
+    self.node_list[-1].next_node = nil
   end
 
   def contains?(value)
-    #TODO
+    for node in self.node_list
+      return true if node.value == value
+    end
+    return false
   end
 
   def find(value)
-    #TODO
+    self.node_list.each_with_index { |node, idx| return idx if node.value == value }
+    return nil
+  end
+
+  def insert_at(value, index)
+    if index == 0
+      self.prepend(value)
+    elsif index == self.node_list.length - 1
+      self.append(value)
+    elsif index > 0 && index < self.node_list.length
+      new_node = Node.new(value)
+      left_sublist = self.node_list[..index - 1]
+      right_sublist = self.node_list[index..]
+      left_sublist[-1].next_node = new_node
+      new_node.next_node = right_sublist[0]
+      left_sublist << new_node
+      self.node_list = left_sublist + right_sublist
+    end
+  end
+
+  def remove_at(index)
+    if index == 0
+      self.node_list.shift
+    elsif index == self.node_list.length - 1
+      self.node_list.pop
+    elsif index > 0 && index < self.node_list.length
+      left_sublist = self.node_list[..index - 1]
+      right_sublist = self.node_list[index + 1..]
+      left_sublist[-1].next_node = right_sublist[0]
+      self.node_list = left_sublist + right_sublist
+    end
   end
 
   def to_s
-    #TODO
+    string_representation = ""
+    for node in self.node_list
+      string_representation += "( #{node.value} ) -> "
+    end
+    string_representation += "nil"
+    string_representation
   end
 end
--- linked-lists/main.rb
+++ linked-lists/main.rb
@@ -3,10 +3,39 @@ require_relative 'lib/linked_list'
 list = LinkedList.new
 
 list.append('dog')
+list.prepend('catz')
 list.append('cat')
 list.append('parrot')
 list.append('hamster')
 list.append('snake')
 list.append('turtle')
 
+puts list.node_list[0]
+puts list.node_list[0].next_node
+puts list.node_list[1]
+
+puts 'Testing #size'
+puts list.size
+
+puts 'Testing #at'
+puts list.at(3).value
+puts list.at(3).next_node
+puts list.at(3).next_node.value
+
+puts 'Testing #contains?'
+puts list.contains?('cat')
+puts list.contains?('cast')
+
+puts 'Testing #find'
+puts "Find 'cat': #{list.find('cat')}"
+puts "Find 'cast': #{list.find('cast')}"
+
+puts list
+
+puts 'Testing #remove_at'
+list.remove_at(3)
+puts list
+
+puts 'Testing #insert_at'
+list.insert_at('papagei',3)
 puts list
